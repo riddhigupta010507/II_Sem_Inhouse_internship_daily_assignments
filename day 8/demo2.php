d2222334545c5deae8db08097b6dd9498d75536d
riddhigupta010507 <riddhigupta1947@gmail.com>
2026-07-10 23:37:22 +0530

    Delete day 8 directory

--- day 8/demo2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$students = ["Amit", "Neha", "Ravi"];
-?>
-
-<ul>
-<?php foreach ($students as $s) { ?>
-    <li><?= $s ?></li>
-<?php } ?>
-</ul>
--- day 8/process.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo $_POST['name'];
-echo"<br>";
-echo $_POST['college'];
-echo"<br>";
-echo $_POST['branch']; die;
-?>
