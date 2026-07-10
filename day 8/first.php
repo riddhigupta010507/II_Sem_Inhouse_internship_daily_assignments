4001c527acae115e5c7d6474ef063cb269407cbe
riddhigupta010507 <riddhigupta1947@gmail.com>
2026-07-10 23:36:48 +0530

    Add files via upload

---
+++ day 8/first.php
@@ -0,0 +1,12 @@
+<?php 
+echo "Hello World", "hello SKIT", " hello Everyone";
+
+echo "<br />";
+print "Hello world" . "<br / > Hello SKIT" . "<br /> Hello everyone";
+//die;
+?>
+
+<?php
+// set the default timezone to use.
+date_default_timezone_set('Asia/kolkata');
+echo date('l jS \of F Y h:i:s A') ?>
